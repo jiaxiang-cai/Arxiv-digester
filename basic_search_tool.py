8a4e1d27e2cdee475d2d40d462271d819ab7b3ab
shiyig233 <131681414+shiyig233@users.noreply.github.com>
2023-06-23 07:46:39 +0300

    Add files via upload

--- basic_search_tool.py
+++ basic_search_tool.py
@@ -2,48 +2,40 @@ import arxiv
 import json
 import openpyxl
 from openpyxl.utils import get_column_letter
+import requests
 
 def arxiv_search(search_term, num_download, criterion, order):
+    # Search for papers
     search = arxiv.Search(
-        query = search_term,
-        max_results = 50, # API limit
-        sort_by = arxiv.SortCriterion.SubmittedDate
+        query=search_term,
+        max_results=num_download,
+        sort_by=criterion,
+        sort_order=order
     )
-    save_result(search)
-    result_download(search, num_download)
 
-def save_result(search):
-    # to get information we wanted, from https://pypi.org/project/arxiv/#example-downloading-papers make a dictionary contain all information for searched result
+    # Saving search results to JSON
     results = []
     for result in search.results():
-        # Extract relevant information from the search result
+        authors = [str(author) for author in result.authors]  # Convert Author object to string
         result_info = {
             'title': result.title,
-            'authors': result.authors,
+            'authors': authors,
             'abstract': result.summary,
-            'upload_date': result.update,
-            # Add more information as needed
+            'upload_date': result.updated.date().isoformat()
         }
         results.append(result_info)
 
-    # then save this dictionary in json file or excel file if needed.
     with open('search_results.json', 'w') as json_file:
         json.dump(results, json_file, indent=4)
-# save in excel 
 
-    save_result_excel(results)
-
-def save_result_excel(results):
+    # Saving search results to Excel
     wb = openpyxl.Workbook()
     ws = wb.active
-
-    # Write the header row
     header = ['Title', 'Authors', 'Abstract', 'Upload Date']
     for col_num, column_title in enumerate(header, 1):
         col_letter = get_column_letter(col_num)
         ws[f"{col_letter}1"] = column_title
 
-    # Write the data rows
     for row_num, result in enumerate(results, 2):
         ws[f"A{row_num}"] = result['title']
         ws[f"B{row_num}"] = ", ".join(result['authors'])
@@ -52,19 +44,11 @@ def save_result_excel(results):
 
     wb.save('search_results.xlsx')
 
-# get url for pdf using result.pdf_url from https://pypi.org/project/arxiv/#example-fetching-results
-import requests
-
-def result_download(search, num_download):
-    import requests
-
-    num_download = 5  # Specify the number of papers to download
-
-    results = search.results()
+    # Download search results
     downloaded_papers = []
     for i in range(num_download):
         try:
-            result = next(results)
+            result = next(search.results())
             pdf_url = result.pdf_url
             response = requests.get(pdf_url)
             downloaded_papers.append(response.content)
@@ -75,7 +59,7 @@ def result_download(search, num_download):
         except Exception as e:
             print(f"Error occurred while downloading paper {i+1}: {str(e)}")
 
-# Save downloaded papers as PDF files
+    # Save downloaded papers as PDF files
     for i, paper in enumerate(downloaded_papers):
         try:
             with open(f"paper_{i+1}.pdf", "wb") as file:
@@ -83,4 +67,7 @@ def result_download(search, num_download):
             print(f"Saved paper {i+1} as PDF file.")
         except Exception as e:
             print(f"Error occurred while saving paper {i+1}: {str(e)}")
+
+# Example usage of the arxiv_search function
+# arxiv_search(search_term="quantum", num_download=10, criterion=arxiv.SortCriterion.SubmittedDate, order=arxiv.SortOrder.Descending)
 
